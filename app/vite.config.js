570b2df975c5d930ce768174493302977c2c9508
Juanseeeee <juanse.5009@gmail.com>
2026-03-13 13:01:43 -0300

    Fix base path for Vercel deployment

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/AssociatesItalia90/',
+  base: '/',
   resolve: {
     alias: {
       'react-native': 'react-native-web'
